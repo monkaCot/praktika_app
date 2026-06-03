66743ccc2d6c1704c9405a13e9d87033d545b7cd
monks <monks@m>
2026-06-03 17:48:50 +0700

    add folder Models and class ServiceRequest

---
+++ FieldService/Models/ServiceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldService.Models
+{
+    public class ServiceRequest
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Problem { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
