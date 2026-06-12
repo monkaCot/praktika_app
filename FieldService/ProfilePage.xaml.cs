58f78cb7cd3e29c5f965fa5056c645993ab299b7
monks <monks@m>
2026-06-13 02:34:15 +0700

    Добавлен профиль.

---
+++ FieldService/ProfilePage.xaml.cs
@@ -0,0 +1,21 @@
+using FieldService.ViewModels;
+
+namespace FieldService;
+
+public partial class ProfilePage : ContentPage
+{
+    private readonly ProfileViewModel _vm;
+
+    public ProfilePage(ProfileViewModel vm)
+    {
+        InitializeComponent();
+        _vm = vm;
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _vm.Refresh();
+    }
+}
---
+++ FieldService/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FieldService.Services;
+using System.Data;
+
+namespace FieldService.ViewModels;
+
+public partial class ProfileViewModel : ObservableObject
+{
+    private readonly AuthService _auth;
+
+    [ObservableProperty] private string _fullName;
+    [ObservableProperty] private string _username;
+    [ObservableProperty] private string _role;
+
+    public ProfileViewModel(AuthService auth)
+    {
+        _auth = auth;
+    }
+
+    public void Refresh()
+    {
+        var u = _auth.CurrentUser;
+        FullName = u?.FullName ?? "";
+        Username = u?.Username ?? "";
+        Role = u?.Role.ToString() ?? "";
+    }
+
+    [RelayCommand]
+    private async Task LogoutAsync()
+    {
+        _auth.Logout();
+        await Shell.Current.GoToAsync("//login");
+    }
+}
