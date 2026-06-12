aed8a4792ad791a608687a9ba60593eb1350086b
monks <monks@m>
2026-06-13 03:08:41 +0700

    убрана возможность создавать заявки от лица техника

--- FieldService/MainPage.xaml.cs
+++ FieldService/MainPage.xaml.cs
@@ -1,18 +1,31 @@
-﻿using FieldService.ViewModels;
+﻿using FieldService.Models;
+using FieldService.Services;
+using FieldService.ViewModels;
 
 namespace FieldService;
 
 public partial class MainPage : ContentPage
 {
-    public MainPage(RequestsViewModel vm)
+    private readonly AuthService _auth;
+
+    public MainPage(RequestsViewModel vm, AuthService auth)
     {
         InitializeComponent();
         BindingContext = vm;
+        _auth = auth;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+
+        bool isTechnician = _auth.CurrentUser?.Role == UserRole.Technician;
+
+        if (isTechnician && ToolbarItems.Contains(AddButton))
+            ToolbarItems.Remove(AddButton);
+        else if (!isTechnician && !ToolbarItems.Contains(AddButton))
+            ToolbarItems.Add(AddButton);
+
         await ((RequestsViewModel)BindingContext).LoadRequestsCommand.ExecuteAsync(null);
     }
 }
