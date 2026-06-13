9ea0e70dcfdc5266b23bc0431d494191aaae8e6d
monks <monks@m>
2026-06-14 01:31:27 +0700

    выполнен дизайн⭐ всех окон

--- FieldService/App.xaml.cs
+++ FieldService/App.xaml.cs
@@ -5,6 +5,7 @@
         public App()
         {
             InitializeComponent();
+            UserAppTheme = AppTheme.Light;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ FieldService/Controls/BrandFooter.xaml.cs
@@ -0,0 +1,9 @@
+namespace FieldService.Controls;
+
+public partial class BrandFooter : ContentView
+{
+    public BrandFooter()
+    {
+        InitializeComponent();
+    }
+}
