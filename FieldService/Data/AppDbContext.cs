f7925051d0572c64a4a10adb6c492a54113e4109
monks <monks@m>
2026-06-14 00:53:31 +0700

    убран неиспользуемый код

--- FieldService/Data/AppDbContext.cs
+++ FieldService/Data/AppDbContext.cs
@@ -11,7 +11,6 @@ namespace FieldService.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<ServiceRequest> Requests { get; set; }
-    public DbSet<DocumentTemplate> Templates { get; set; }
     public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
